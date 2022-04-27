4b20b92d03f7c24a84c484cd1381fe450a5b4f2a
kkenjeres <kkenjeres96@gmail.com>
2022-04-27 11:38:01 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,167 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
+    <preloader />
+    <main>
+      <div class="search-box">
+        <input 
+        type="text" 
+        class="search-bar" 
+        placeholder="Wheather in" 
+        v-model="query"
+        @keypress="fetchWeather"
+        />
+      </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="temp">feels like{{ Math.round(weather.main.feels_like) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import preloader from './preloader.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    preloader
+  },
+  name: 'app',
+  data() {
+    return{
+      api_key: 'c0eb90c144ec33a0aee2cd45a7d873f1', 
+      url_base: "https://api.openweathermap.org/data/2.5/", // обращаюсь на серв "https://api.openweathermap.org/data/2.5/weather?q=?&units=metric&APPID=c0eb90c144ec33a0aee2cd45a7d873f1" 
+      query: "" ,
+      weather: {}
+    }
+  },
+  methods:{
+    fetchWeather(e) {
+      if(e.key =="Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+          this.query = "";
+      }
+    },
+    
+    setResults (results) {
+      this.weather = results;
+  },
+  dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
+    }
   }
 }
 </script>
-
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+            let d = new Date();
+           
+            let day = days[d.getDay()];
+            document.getElementById("demo").innerHTML = day;
+          }       
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  font-family: 'montserrat', sans-serif;
+  font-size: 15px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  background: url('./assets/img/cloud.jpg');
+  background-size: cover;
+  background-position: center;
+  transition-delay: 0.7s;
+}
+#app.warm{
+  transition: 0.9s;
+  background: url('./assets/img/sunset.jpg');
+  background-position: center;
+}
+main{
+  min-height: 100vh;
+  padding: 25px;
+  
+}
+.search-box{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 40px 0 100px 0;
+}
+.search-bar{
+  width: 60%;
+  padding: 15px;
+  font-size: 20px;
+  font-style: italic;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.25);
+  color: #313131;
+  border: none;
+  outline: none;
+  border-radius: 0 25px 0 25px;
+  background-color: rgba(255, 255, 255, 0.7);
+  transition: 0.4s;
+}
+
+.search-bar:focus{
+  box-shadow: 0 0 16px rgba(0, 0, 0, 0.5);
+  background-color: rgba(255, 255, 255, 0.85);
+  border-radius: 25px 0px 25px 0px;
+}
+.location{
+  margin-bottom: 10px;
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.date{
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.temp{
+  width: 60%;
+  text-align: center;
+  margin: 30px auto;
+  font-size: 102px;
+  font-weight: 900;
+  color: #fff;
+  padding: 10px;
+  background-color: rgba(0, 0, 0, 0.226);
+  border-radius: 15px;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.weather{
+  text-align: center;
+  font-size: 50px;
+  font-weight: 700;
+  font-style: italic;
+  color: #fff;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
---
+++ src/preloader.vue
@@ -0,0 +1,81 @@
+<template>
+    <div v-if="show" class="preloader">
+        <p>CHECK THE WEATHER</p>
+        <div class="lds-ripple"><div></div><div></div></div>
+    </div>
+</template>
+
+<script> 
+export default {
+    name: 'preloader',
+    data(){
+        return{
+            show: true
+        }
+    },
+    mounted(){
+        this.showToggle()
+     },
+        methods:{
+            showToggle(){
+                setTimeout(() => {
+                    this.show = false
+                }, 3000)
+            }
+       }
+}
+</script>
+
+<style>
+
+.preloader{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.8)),
+    url('../src/assets/img/statya.jpg') bottom no-repeat,
+    url('../src/assets/img/triangle.jpg');
+}
+p{
+    margin-bottom: 40px;
+    font-size: 72px;
+    font-weight: medium;
+    background: #ffffff;
+}
+.lds-ripple {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ripple div {
+  position: absolute;
+  border: 4px solid #fff;
+  opacity: 1;
+  border-radius: 50%;
+  animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+}
+.lds-ripple div:nth-child(2) {
+  animation-delay: -0.5s;
+}
+@keyframes lds-ripple {
+  0% {
+    top: 36px;
+    left: 36px;
+    width: 0;
+    height: 0;
+    opacity: 1;
+  }
+  100% {
+    top: 0px;
+    left: 0px;
+    width: 72px;
+    height: 72px;
+    opacity: 0;
+  }
+}
+</style>
